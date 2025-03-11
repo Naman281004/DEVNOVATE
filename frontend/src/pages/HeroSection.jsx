7ab05c199d3c52b317590cc6309c6910555ff7e7
yume <yume@me.com>
2025-03-11 20:33:22 +0530

    initial

--- frontend/src/pages/HeroSection.jsx
+++ frontend/src/pages/HeroSection.jsx
@@ -4,17 +4,17 @@ import { AuroraBackground } from '../components/aurora-background'
 export default function HeroSection() {
   return (
     <div className="h-screen w-full">
-      <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+      <header className="p-4 flex bg-transparent justify-end items-center fixed right-0 top-0 z-10 text-slate-300">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <AuroraBackground>
         <div className="flex flex-col items-center justify-center h-full w-full text-white">
-          <h1 className="text-7xl font-bold mb-4">Devnovate</h1>
+          <h1 className="text-9xl font-bold mb-4">Devnovate</h1>
         </div>
       </AuroraBackground>
     </div>
